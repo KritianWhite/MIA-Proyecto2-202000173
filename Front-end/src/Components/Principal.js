8fc063606e0f5e368354bedbad98b730f76b9dad
Christian Blanco <3617836980101@ingenieria.usac.edu.gt>
2023-04-06 20:57:08 -0600

    ...

--- Front-end/src/Components/Principal.js
+++ Front-end/src/Components/Principal.js
@@ -22,29 +22,28 @@ const Consoles2 = () => {
             <h2>Editor de código</h2>
             <textarea
               ref={textAreaRef}
-              rows={25}
+              rows={20}
               cols={80}
               className="code-textarea"
             />
-          </div>
-
-          <div className="console">
-            <h2>Consola</h2>
-            <textarea rows={25} cols={80} className="console-textarea" />
-          </div>
-
-          <input
+            <input
             onChange={handleFileInputChange}
             class="form-control form-control-lg"
             id="formFileLg"
             type="file"
           />
+          </div>
 
-          <div class="d-grid gap-2">
-            <button type="button" class="btn btn-light">
+          <div className="console">
+            <h2>Consola</h2>
+            <textarea rows={20} cols={80} className="console-textarea" readOnly />
+            <div class="d-grid gap-2">
+            <button className="btn-ejecutar" type="button" class="btn btn-light">
               Ejecutar
             </button>
           </div>
+          </div>
+
         </div>
       </div>
     </>
