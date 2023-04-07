877cc41d4f8b7941978647ac5936480a6a417ff0
Christian Blanco <3617836980101@ingenieria.usac.edu.gt>
2023-04-06 19:16:08 -0600

    Editores listos, carga y muestra de archivos.

--- Front-end/src/Components/Principal.js
+++ Front-end/src/Components/Principal.js
@@ -1,58 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../Styles/Principal.css";
 
-const CodeEditor = () => {
-  const [code, setCode] = React.useState("");
+const Consoles2 = () => {
+  const textAreaRef = useRef(null);
 
-  const handleCodeChange = (event) => {
-    setCode(event.target.value);
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const content = e.target.result;
+      textAreaRef.current.value = content;
+    };
+    reader.readAsText(file);
   };
 
   return (
-    <div className="code-editor">
-      <h2>Editor de código</h2>
+    <>
+      <div className="principal">
+        <div className="container">
+          <div className="code-editor">
+            <h2>Editor de código</h2>
+            <textarea
+              ref={textAreaRef}
+              rows={25}
+              cols={80}
+              className="code-textarea"
+            />
+          </div>
 
-      <textarea
-        rows={25}
-        cols={80}
-        value={code}
-        onChange={handleCodeChange}
-        className="code-textarea"
-      />
-    </div>
-  );
-};
+          <div className="console">
+            <h2>Consola</h2>
+            <textarea rows={25} cols={80} className="console-textarea" />
+          </div>
 
-const Console = () => {
-  const [output, setOutput] = React.useState("");
+          <input
+            onChange={handleFileInputChange}
+            class="form-control form-control-lg"
+            id="formFileLg"
+            type="file"
+          />
 
-  const handleOutputChange = (event) => {
-    setOutput(event.target.value);
-  };
-
-  return (
-    <div className="console">
-      <h2>Consola</h2>
-      <textarea
-        rows={25}
-        cols={80}
-        value={output}
-        onChange={handleOutputChange}
-        className="console-textarea"
-      />
-    </div>
-  );
-};
-
-const Consoles = () => {
-  return (
-    <div className="principal">
-      <div className="container">
-        <CodeEditor />
-        <Console />
+          <div class="d-grid gap-2">
+            <button type="button" class="btn btn-light">
+              Ejecutar
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default Consoles;
+export default Consoles2;
